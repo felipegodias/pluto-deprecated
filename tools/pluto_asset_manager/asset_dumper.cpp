4e3495b1ffe2032941cbdf3f9dd8f00d56087f68
felipegodias <felipe.g.o.dias@outlook.com>
2019-07-21 06:32:51 -0300

    Created header to dump asset easly in the asset manager.

---
+++ tools/pluto_asset_manager/asset_dumper.cpp
@@ -0,0 +1,30 @@
+#include "asset_dumper.h"
+
+#include <pluto/di/di_container.h>
+#include <pluto/file/file_reader.h>
+#include <pluto/file/file_writer.h>
+#include <pluto/file/file_manager.h>
+#include <pluto/file/path.h>
+#include <pluto/asset/asset.h>
+#include <pluto/guid.h>
+#include <iostream>
+
+namespace pluto
+{
+    void DumpAsset(const Path& path, const Asset& asset)
+    {
+        DiContainer diContainer;
+        diContainer.AddSingleton(std::make_unique<FileWriter::Factory>(diContainer));
+        diContainer.AddSingleton(std::make_unique<FileReader::Factory>(diContainer));
+
+        const FileManager::Factory fileManagerFactory(diContainer);
+        const std::unique_ptr<FileManager> fileManager = fileManagerFactory.Create(path);
+
+        const std::string guidStr = asset.GetId().Str();
+
+        const auto fileWriter = fileManager->OpenWrite(Path(path.Str() + "/" + guidStr));
+        asset.Dump(*fileWriter);
+
+        std::cout << "Asset \"" << asset.GetName() << "\" saved with id " << guidStr << "." << std::endl;
+    }
+}
---
+++ tools/pluto_asset_manager/asset_dumper.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <pluto/file/file_manager.h>
+#include <pluto/asset/asset.h>
+
+namespace pluto
+{
+    void DumpAsset(const Path& path, const Asset& asset);
+}
