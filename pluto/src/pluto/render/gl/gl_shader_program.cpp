8dfdc3cf86d7b7f1e98451e0afc1b3b379bc26e4
felipegodias <felipe.g.o.dias@outlook.com>
2019-07-26 02:25:52 -0300

    OpenGL shader program now has support for new shader asset.

--- pluto/src/pluto/render/gl/gl_shader_program.cpp
+++ pluto/src/pluto/render/gl/gl_shader_program.cpp
@@ -22,7 +22,7 @@
 
 namespace pluto
 {
-    static const std::array<GLenum, static_cast<int>(ShaderAsset::BlendFunction::Count)> BLEND_FUNCTIONS{
+    static const std::array<GLenum, static_cast<int>(ShaderAsset::BlendEquation::Count)> BLEND_EQUATIONS{
         GL_NONE,
         GL_FUNC_ADD,
         GL_FUNC_SUBTRACT,
@@ -35,38 +35,37 @@ namespace pluto
         GL_ZERO,
         GL_ONE,
         GL_SRC_COLOR,
-        GL_DST_COLOR,
-        GL_SRC_ALPHA,
-        GL_DST_ALPHA,
         GL_ONE_MINUS_SRC_COLOR,
+        GL_DST_COLOR,
         GL_ONE_MINUS_DST_COLOR,
+        GL_SRC_ALPHA,
         GL_ONE_MINUS_SRC_ALPHA,
+        GL_DST_ALPHA,
         GL_ONE_MINUS_DST_ALPHA,
     };
 
-    static const std::array<GLenum, static_cast<int>(ShaderAsset::ZTest::Count)> DEPTH_TESTS{
+    static const std::array<GLenum, static_cast<int>(ShaderAsset::DepthTest::Count)> DEPTH_TESTS{
+        GL_NONE,
+        GL_ALWAYS,
         GL_NEVER,
-        GL_NEVER,
-        GL_LESS,
         GL_LEQUAL,
         GL_EQUAL,
+        GL_LEQUAL,
+        GL_GREATER,
         GL_NOTEQUAL,
         GL_GEQUAL,
-        GL_GREATER,
-        GL_ALWAYS,
     };
 
-    static const std::array<GLenum, static_cast<int>(ShaderAsset::CullMode::Count)> FACE_CULLING{
+    static const std::array<GLenum, static_cast<int>(ShaderAsset::CullFace::Count)> FACE_CULLING{
+        GL_NONE,
         GL_FRONT,
         GL_BACK,
-        GL_FRONT_AND_BACK,
     };
 
     class GlShaderProgram::Impl
     {
-    private:
         const GLuint programId;
-        const ShaderAsset& shaderAsset;
+        const ShaderAsset* shaderAsset;
 
         uint8_t mvpUniformLocation;
 
@@ -74,11 +73,14 @@ namespace pluto
         const MaterialAsset* lastMaterialAsset;
 
     public:
-        Impl(const GLuint programId, const ShaderAsset& shaderAsset) : programId(programId), shaderAsset(shaderAsset),
-                                                                       mvpUniformLocation(255), isBound(false),
-                                                                       lastMaterialAsset(nullptr)
+        Impl(const GLuint programId, const ShaderAsset& shaderAsset)
+            : programId(programId),
+              shaderAsset(&shaderAsset),
+              mvpUniformLocation(255),
+              isBound(false),
+              lastMaterialAsset(nullptr)
         {
-            for (const auto& property : shaderAsset.GetProperties())
+            for (const auto& property : shaderAsset.GetUniforms())
             {
                 if (property.name != "u_mvp")
                 {
@@ -103,7 +105,8 @@ namespace pluto
                 GL_CALL(glUseProgram(programId));
                 UpdateBlendFunction();
                 UpdateDepthTest();
-                UpdateFaceCulling();
+                UpdateFaceCull();
+                UpdateAttributes();
                 isBound = true;
             }
 
@@ -126,19 +129,44 @@ namespace pluto
     private:
         void UpdateBlendFunction()
         {
-            const bool isBlendOn = glIsEnabled(GL_BLEND);
-            if (shaderAsset.GetBlendFunction() != ShaderAsset::BlendFunction::None)
+            GL_CALL(const bool isBlendOn = glIsEnabled(GL_BLEND));
+            if (shaderAsset->GetBlendEquation() != ShaderAsset::BlendEquation::Off)
             {
                 if (!isBlendOn)
                 {
                     GL_CALL(glEnable(GL_BLEND));
                 }
 
-                const GLenum srcBlendFactor = BLEND_FACTORS[static_cast<int>(shaderAsset.GetSrcBlendFactor())];
-                const GLenum dstBlendFactor = BLEND_FACTORS[static_cast<int>(shaderAsset.GetDstBlendFactor())];
-                GL_CALL(glBlendFunc(srcBlendFactor, dstBlendFactor));
+                const GLenum blendSrcFactor = BLEND_FACTORS[static_cast<int>(shaderAsset->GetBlendSrcFactor())];
+                const GLenum blendDstFactor = BLEND_FACTORS[static_cast<int>(shaderAsset->GetBlendDstFactor())];
 
-                GL_CALL(glBlendEquation(BLEND_FUNCTIONS[static_cast<int>(shaderAsset.GetBlendFunction())]));
+                const GLenum blendSrcAlphaFactor = BLEND_FACTORS[static_cast<int>(shaderAsset->GetBlendSrcAlphaFactor())
+                ];
+
+                const GLenum blendDstAlphaFactor = BLEND_FACTORS[static_cast<int>(shaderAsset->GetBlendDstAlphaFactor())
+                ];
+
+                if (blendSrcFactor == blendSrcAlphaFactor && blendDstFactor == blendDstAlphaFactor)
+                {
+                    GL_CALL(glBlendFunc(blendSrcFactor, blendDstFactor));
+                }
+                else
+                {
+                    GL_CALL(
+                        glBlendFuncSeparate(blendSrcFactor, blendDstFactor, blendSrcAlphaFactor, blendDstAlphaFactor));
+                }
+
+                const GLenum blendEquation = BLEND_EQUATIONS[static_cast<int>(shaderAsset->GetBlendEquation())];
+                const GLenum blendAlphaEquation = BLEND_EQUATIONS[static_cast<int>(shaderAsset->GetBlendAlphaEquation())
+                ];
+                if (blendEquation == blendAlphaEquation)
+                {
+                    GL_CALL(glBlendEquation(blendEquation));
+                }
+                else
+                {
+                    GL_CALL(glBlendEquationSeparate(blendEquation, blendAlphaEquation));
+                }
             }
             else if (isBlendOn)
             {
@@ -148,36 +176,65 @@ namespace pluto
 
         void UpdateDepthTest()
         {
-            const bool isZTestOn = glIsEnabled(GL_DEPTH_TEST);
-            if (shaderAsset.GetZTest() != ShaderAsset::ZTest::None)
+            GL_CALL(const bool isDepthTestOn = glIsEnabled(GL_DEPTH_TEST));
+            if (shaderAsset->GetDepthTest() != ShaderAsset::DepthTest::Off)
             {
-                if (!isZTestOn)
+                if (!isDepthTestOn)
                 {
                     GL_CALL(glEnable(GL_DEPTH_TEST));
                 }
-                GL_CALL(glDepthFunc(DEPTH_TESTS[static_cast<int>(shaderAsset.GetZTest())]));
+
+                GL_CALL(glDepthFunc(DEPTH_TESTS[static_cast<int>(shaderAsset->GetDepthTest())]));
             }
-            else if (isZTestOn)
+            else if (isDepthTestOn)
             {
                 GL_CALL(glDisable(GL_DEPTH_TEST));
             }
         }
 
-        void UpdateFaceCulling()
+        void UpdateFaceCull()
         {
-            GL_CALL(glCullFace(FACE_CULLING[static_cast<int>(shaderAsset.GetCullMode())]));
+            GL_CALL(const bool isFaceCullOn = glIsEnabled(GL_CULL_FACE));
+            if (shaderAsset->GetCullFace() != ShaderAsset::CullFace::Off)
+            {
+                if (!isFaceCullOn)
+                {
+                    GL_CALL(glEnable(GL_CULL_FACE));
+                }
+
+                GL_CALL(glCullFace(FACE_CULLING[static_cast<int>(shaderAsset->GetCullFace())]));
+            }
+            else if (isFaceCullOn)
+            {
+                GL_CALL(glDisable(GL_DEPTH_TEST));
+            }
+        }
+
+        void UpdateAttributes()
+        {
+            for (const auto& uniform : shaderAsset->GetUniforms())
+            {
+                if (uniform.name == "vertex.pos")
+                {
+                    glBindAttribLocation(programId, 0, "vertex.pos");
+                }
+                else if (uniform.name == "vertex.uv")
+                {
+                    glBindAttribLocation(programId, 1, "vertex.uv");
+                }
+            }
         }
 
         void UpdateMaterial()
         {
-            for (const auto& property : shaderAsset.GetProperties())
+            for (const auto& uniform : shaderAsset->GetUniforms())
             {
-                if (property.id == mvpUniformLocation)
+                if (uniform.id == mvpUniformLocation)
                 {
                     continue;
                 }
 
-                UpdateProperty(property);
+                UpdateUniform(uniform);
             }
         }
 
@@ -186,77 +243,79 @@ namespace pluto
             GL_CALL(glUniformMatrix4fv(mvpUniformLocation, 1, GL_FALSE, mvp.Data()));
         }
 
-    private:
-        void UpdateProperty(const ShaderAsset::Property& property)
+        void UpdateUniform(const ShaderAsset::Property& uniform)
         {
-            switch (property.type)
+            switch (uniform.type)
             {
             case ShaderAsset::Property::Type::Bool:
-                GL_CALL(glUniform1i(property.id, lastMaterialAsset->GetBool(property.name)));
+                GL_CALL(glUniform1i(uniform.id, lastMaterialAsset->GetBool(uniform.name)));
                 break;
             case ShaderAsset::Property::Type::Int:
-                GL_CALL(glUniform1i(property.id, lastMaterialAsset->GetInt(property.name)));
+                GL_CALL(glUniform1i(uniform.id, lastMaterialAsset->GetInt(uniform.name)));
                 break;
             case ShaderAsset::Property::Type::Float:
-                GL_CALL(glUniform1f(property.id, lastMaterialAsset->GetFloat(property.name)));
+                GL_CALL(glUniform1f(uniform.id, lastMaterialAsset->GetFloat(uniform.name)));
                 break;
             case ShaderAsset::Property::Type::Vector2I:
-                GL_CALL(glUniform2iv(property.id, 1, lastMaterialAsset->GetVector2I(property.name).Data()));
+                GL_CALL(glUniform2iv(uniform.id, 1, lastMaterialAsset->GetVector2I(uniform.name).Data()));
                 break;
             case ShaderAsset::Property::Type::Vector2F:
-                GL_CALL(glUniform2fv(property.id, 1, lastMaterialAsset->GetVector2F(property.name).Data()));
+                GL_CALL(glUniform2fv(uniform.id, 1, lastMaterialAsset->GetVector2F(uniform.name).Data()));
                 break;
             case ShaderAsset::Property::Type::Vector3I:
-                GL_CALL(glUniform3iv(property.id, 1, lastMaterialAsset->GetVector3I(property.name).Data()));
+                GL_CALL(glUniform3iv(uniform.id, 1, lastMaterialAsset->GetVector3I(uniform.name).Data()));
                 break;
             case ShaderAsset::Property::Type::Vector3F:
-                GL_CALL(glUniform3fv(property.id, 1, lastMaterialAsset->GetVector3F(property.name).Data()));
+                GL_CALL(glUniform3fv(uniform.id, 1, lastMaterialAsset->GetVector3F(uniform.name).Data()));
                 break;
             case ShaderAsset::Property::Type::Vector4I:
-                GL_CALL(glUniform4iv(property.id, 1, lastMaterialAsset->GetVector4I(property.name).Data()));
+                GL_CALL(glUniform4iv(uniform.id, 1, lastMaterialAsset->GetVector4I(uniform.name).Data()));
                 break;
             case ShaderAsset::Property::Type::Vector4F:
-                GL_CALL(glUniform4fv(property.id, 1, lastMaterialAsset->GetVector4F(property.name).Data()));
+                GL_CALL(glUniform4fv(uniform.id, 1, lastMaterialAsset->GetVector4F(uniform.name).Data()));
                 break;
             case ShaderAsset::Property::Type::Matrix4X4:
                 GL_CALL(
-                    glUniformMatrix3fv(property.id, 1, GL_FALSE, lastMaterialAsset->GetMatrix4X4(property.name).Data()
+                    glUniformMatrix3fv(uniform.id, 1, GL_FALSE, lastMaterialAsset->GetMatrix4X4(uniform.name).Data()
                     ));
                 break;
             case ShaderAsset::Property::Type::Sampler2D:
-                BindTexture(property);
+                BindTexture(uniform);
                 break;
             default: ;
             }
         }
 
-        void BindTexture(const ShaderAsset::Property& property)
+        void BindTexture(const ShaderAsset::Property& uniform)
         {
-            TextureAsset& textureAsset = lastMaterialAsset->GetTexture(property.name);
+            TextureAsset& textureAsset = lastMaterialAsset->GetTexture(uniform.name);
             auto& textureBuffer = dynamic_cast<GlTextureBuffer&>(textureAsset.GetTextureBuffer());
             textureBuffer.Bind(0);
-            GL_CALL(glUniform1i(property.id, 0));
+            GL_CALL(glUniform1i(uniform.id, 0));
         }
     };
 
-    GlShaderProgram::Factory::Factory(DiContainer& diContainer) : ShaderProgram::Factory(diContainer)
+    GlShaderProgram::Factory::Factory(DiContainer& diContainer)
+        : ShaderProgram::Factory(diContainer)
     {
     }
 
     std::unique_ptr<ShaderProgram> GlShaderProgram::Factory::Create(const ShaderAsset& shaderAsset) const
     {
         GL_CALL(const GLuint programId = glCreateProgram());
-        const std::vector<uint8_t>& binary = shaderAsset.GetBinary();
-        GL_CALL(glProgramBinary(programId, shaderAsset.GetBinaryFormat(), binary.data(), binary.size()));
+        const std::vector<uint8_t>& binaryData = shaderAsset.GetBinaryData();
+        GL_CALL(glProgramBinary(programId, shaderAsset.GetBinaryFormat(), binaryData.data(), binaryData.size()));
 
         return std::make_unique<GlShaderProgram>(std::make_unique<Impl>(programId, shaderAsset));
     }
 
-    GlShaderProgram::GlShaderProgram(std::unique_ptr<Impl> impl) : impl(std::move(impl))
+    GlShaderProgram::GlShaderProgram(std::unique_ptr<Impl> impl)
+        : impl(std::move(impl))
     {
     }
 
-    GlShaderProgram::GlShaderProgram(GlShaderProgram&& other) noexcept : impl(std::move(other.impl))
+    GlShaderProgram::GlShaderProgram(GlShaderProgram&& other) noexcept
+        : impl(std::move(other.impl))
     {
     }
 
