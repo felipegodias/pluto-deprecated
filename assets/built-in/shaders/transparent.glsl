25010fb840f46aecba7dca14e10ee7171465c717
felipegodias <felipe.g.o.dias@outlook.com>
2019-07-27 01:56:27 -0300

    Created transparent shader.

---
+++ assets/built-in/shaders/transparent.glsl
@@ -0,0 +1,50 @@
+#version 330 core
+
+#define PLUTO_BLEND SRC_ALPHA ONE_MINUS_SRC_ALPHA
+#define PLUTO_BLEND_EQUATION ADD
+
+struct V2F
+{
+    vec2 texCoord;
+};
+
+#ifdef PLUTO_VERTEX_SHADER
+
+struct VertexData
+{
+    vec3 pos;
+    vec2 uv;
+};
+
+in VertexData vertex;
+
+uniform mat4 u_mvp;
+
+out V2F v2f;
+
+void main()
+{
+    gl_Position = u_mvp * vec4(vertex.pos, 1);
+    v2f.texCoord = vertex.uv;
+}
+
+#endif
+
+#ifdef PLUTO_FRAGMENT_SHADER
+
+struct MaterialData
+{
+    sampler2D mainTex;
+};
+
+uniform MaterialData u_mat;
+
+in V2F v2f;
+
+out vec4 outColor;
+
+void main()
+{
+    outColor = texture(u_mat.mainTex, v2f.texCoord);
+}
+#endif
