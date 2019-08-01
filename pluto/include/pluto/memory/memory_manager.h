226bfec839c967dc9cd274d723a02eeffab59fa5
felipegodias <felipe.g.o.dias@outlook.com>
2019-08-01 01:12:54 -0300

    Created headers for the memory module.

---
+++ pluto/include/pluto/memory/memory_manager.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "pluto/api.h"
+#include "pluto/service/base_service.h"
+#include "pluto/service/base_factory.h"
+
+#include <memory>
+
+namespace pluto
+{
+    class Object;
+
+    class PLUTO_API MemoryManager final : public BaseService
+    {
+    public:
+        class PLUTO_API Factory final : public BaseFactory
+        {
+        public:
+            explicit Factory(ServiceCollection& serviceCollection);
+        };
+
+    private:
+        class Impl;
+        std::unique_ptr<Impl> impl;
+
+    public:
+        ~MemoryManager();
+        explicit MemoryManager(std::unique_ptr<Impl> impl);
+
+        MemoryManager(const MemoryManager& other) = delete;
+        MemoryManager(MemoryManager&& other) noexcept;
+        MemoryManager& operator=(const MemoryManager& rhs) = delete;
+        MemoryManager& operator=(MemoryManager&& rhs) noexcept;
+
+        void Register(std::unique_ptr<Object> object);
+        Object* Get(size_t instanceId);
+    };
+}
---
+++ pluto/include/pluto/memory/object.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "pluto/api.h"
+#include <memory>
+
+namespace pluto
+{
+    class Guid;
+    class LazyPtr;
+
+    class PLUTO_API Object
+    {
+    public:
+        virtual ~Object() = 0;
+        Object();
+
+        Object(const Object& other) = delete;
+        Object(Object&& other) noexcept;
+        Object& operator=(const Object& rhs) = delete;
+        Object& operator=(Object&& rhs) noexcept;
+
+        virtual const Guid& GetId() const = 0;
+
+        virtual std::shared_ptr<LazyPtr> GetPtr() const = 0;
+        virtual void SetPtr(std::shared_ptr<LazyPtr> value) = 0;
+
+        bool operator==(const Object& rhs) const;
+        bool operator!=(const Object& rhs) const;
+    };
+}
