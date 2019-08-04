060923370260b21deba2e826c200b71ba2488c0d
felipegodias <felipe.g.o.dias@outlook.com>
2019-08-04 02:26:58 -0300

    Refactored resource.h.

--- pluto/include/pluto/memory/resource.h
+++ pluto/include/pluto/memory/resource.h
@@ -7,14 +7,6 @@
 
 namespace pluto
 {
-    class Object;
-
-    template <typename T1, typename T2>
-    using IsBaseOf = std::enable_if_t<std::is_base_of_v<T1, T2>, bool>;
-
-    template <typename T>
-    using IsObject = std::enable_if_t<std::is_base_of_v<Object, T>, bool>;
-
     template <typename T, typename Enable = void>
     class PLUTO_API Resource;
 
@@ -26,106 +18,143 @@ namespace pluto
 
         std::shared_ptr<ResourceControl> control;
 
-        explicit Resource(std::unique_ptr<ResourceControl> control)
-            : control(control.release())
-        {
-        }
+        explicit Resource(std::unique_ptr<ResourceControl> control);
 
     public:
-        Resource()
-        {
-        }
+        Resource();
+        Resource(nullptr_t);
 
-        Resource(nullptr_t)
-        {
-        }
+        template <typename T2, std::enable_if_t<std::is_base_of_v<T, T2>, bool>  = false>
+        Resource(const Resource<T2>& other);
+        template <typename T2, std::enable_if_t<std::is_base_of_v<T, T2>, bool>  = false>
+        Resource(Resource<T2>&& other) noexcept;
+        template <typename T2, std::enable_if_t<std::is_base_of_v<T, T2>, bool>  = false>
+        Resource& operator=(const Resource<T2>& rhs);
+        template <typename T2, std::enable_if_t<std::is_base_of_v<T, T2>, bool>  = false>
+        Resource& operator=(Resource<T2>&& rhs);
 
-        template <typename T2, IsBaseOf<T, T2>  = 0>
-        Resource(const Resource<T2>& other)
-            : control(other.control)
-        {
-        }
+        bool operator==(nullptr_t) const;
+        bool operator!=(nullptr_t) const;
 
-        template <typename T2, IsBaseOf<T, T2>  = 0>
-        Resource(Resource<T2>&& other) noexcept
-            : control(std::move(other.control))
-        {
-        }
+        const T* Get() const;
+        T* Get();
 
-        template <typename T2, IsBaseOf<T, T2>  = 0>
-        Resource& operator=(const Resource<T2>& rhs)
-        {
-            control = rhs.control;
-            return *this;
-        }
-
-        template <typename T2, IsBaseOf<T, T2>  = 0>
-        Resource& operator=(Resource<T2>&& rhs)
-        {
-            control = std::move(rhs.control);
-            return *this;
-        }
-
-        bool operator==(nullptr_t) const
-        {
-            return control == nullptr || control->Get() == nullptr;
-        }
-
-        bool operator!=(nullptr_t) const
-        {
-            return !(*this == nullptr);
-        }
-
-        const T* Get() const
-        {
-            if (control == nullptr)
-            {
-                return nullptr;
-            }
-            return dynamic_cast<const T*>(control->Get());
-        }
-
-        T* Get()
-        {
-            if (control == nullptr)
-            {
-                return nullptr;
-            }
-            return dynamic_cast<T*>(control->Get());
-        }
-
-        const T* operator->() const
-        {
-            if (control == nullptr)
-            {
-                return nullptr;
-            }
-            return dynamic_cast<const T*>(control->Get());
-        }
-
-        T* operator->()
-        {
-            if (control == nullptr)
-            {
-                return nullptr;
-            }
-            return dynamic_cast<T*>(control->Get());
-        }
+        const T* operator->() const;
+        T* operator->();
     };
 
-    class ResourceUtils
+    class PLUTO_API ResourceUtils
     {
     public:
-        template <typename T1, typename T2, IsObject<T2>  = 0, IsBaseOf<T2, T1>  = 0>
-        static Resource<T1> Cast(const Resource<T2>& resource)
-        {
-            if (dynamic_cast<const T1*>(resource.Get()) == nullptr)
-            {
-                return nullptr;
-            }
-            Resource<T1> out;
-            out.control = resource.control;
-            return out;
-        }
+        template <typename T1,
+                  typename T2,
+                  std::enable_if_t<std::is_base_of_v<Object, T2>, bool>  = false,
+                  std::enable_if_t<std::is_base_of_v<T2, T1>, bool>  = false>
+        static Resource<T1> Cast(const Resource<T2>& resource);
     };
+
+    template <typename T>
+    Resource<T, std::enable_if_t<std::is_base_of_v<Object, T>>>::Resource(std::unique_ptr<ResourceControl> control)
+        : control(control.release())
+    {
+    }
+
+    template <typename T>
+    Resource<T, std::enable_if_t<std::is_base_of_v<Object, T>>>::Resource() = default;
+
+    template <typename T>
+    Resource<T, std::enable_if_t<std::is_base_of_v<Object, T>>>::Resource(nullptr_t)
+    {
+    }
+
+    template <typename T>
+    template <typename T2, std::enable_if_t<std::is_base_of_v<T, T2>, bool>>
+    Resource<T, std::enable_if_t<std::is_base_of_v<Object, T>>>::Resource(const Resource<T2>& other)
+        : control(other.control)
+    {
+    }
+
+    template <typename T>
+    template <typename T2, std::enable_if_t<std::is_base_of_v<T, T2>, bool>>
+    Resource<T, std::enable_if_t<std::is_base_of_v<Object, T>>>::Resource(Resource<T2>&& other) noexcept
+        : control(std::move(other.control))
+    {
+    }
+
+    template <typename T>
+    template <typename T2, std::enable_if_t<std::is_base_of_v<T, T2>, bool>>
+    Resource<T, std::enable_if_t<std::is_base_of_v<Object, T>>>& Resource<
+        T, std::enable_if_t<std::is_base_of_v<Object, T>>>::operator=(const Resource<T2>& rhs)
+    {
+        control = rhs.control;
+        return *this;
+    }
+
+    template <typename T>
+    template <typename T2, std::enable_if_t<std::is_base_of_v<T, T2>, bool>>
+    Resource<T, std::enable_if_t<std::is_base_of_v<Object, T>>>& Resource<
+        T, std::enable_if_t<std::is_base_of_v<Object, T>>>::operator=(Resource<T2>&& rhs)
+    {
+        control = std::move(rhs.control);
+        return *this;
+    }
+
+    template <typename T>
+    bool Resource<T, std::enable_if_t<std::is_base_of_v<Object, T>>>::operator==(nullptr_t) const
+    {
+        return !control || control->Get() == nullptr;
+    }
+
+    template <typename T>
+    bool Resource<T, std::enable_if_t<std::is_base_of_v<Object, T>>>::operator!=(nullptr_t) const
+    {
+        return !(*this == nullptr);
+    }
+
+    template <typename T>
+    const T* Resource<T, std::enable_if_t<std::is_base_of_v<Object, T>>>::Get() const
+    {
+        if (control == nullptr)
+        {
+            return nullptr;
+        }
+        return dynamic_cast<const T*>(control->Get());
+    }
+
+    template <typename T>
+    T* Resource<T, std::enable_if_t<std::is_base_of_v<Object, T>>>::Get()
+    {
+        if (control == nullptr)
+        {
+            return nullptr;
+        }
+        return dynamic_cast<T*>(control->Get());
+    }
+
+    template <typename T>
+    const T* Resource<T, std::enable_if_t<std::is_base_of_v<Object, T>>>::operator->() const
+    {
+        return Get();
+    }
+
+    template <typename T>
+    T* Resource<T, std::enable_if_t<std::is_base_of_v<Object, T>>>::operator->()
+    {
+        return Get();
+    }
+
+    template <typename T1,
+              typename T2,
+              std::enable_if_t<std::is_base_of_v<Object, T2>, bool>,
+              std::enable_if_t<std::is_base_of_v<T2, T1>, bool>>
+    Resource<T1> ResourceUtils::Cast(const Resource<T2>& resource)
+    {
+        if (dynamic_cast<const T1*>(resource.Get()) == nullptr)
+        {
+            return nullptr;
+        }
+        Resource<T1> out;
+        out.control = resource.control;
+        return out;
+    }
 }
