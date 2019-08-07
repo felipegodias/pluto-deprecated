932d914eae884122f784a2850220af3328795b3b
felipegodias <felipe.g.o.dias@outlook.com>
2019-08-07 02:32:41 -0300

    Created font asset header.

---
+++ pluto/include/pluto/asset/font_asset.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "asset.h"
+#include "pluto/service/base_factory.h"
+
+namespace pluto
+{
+    template <typename T, typename Enable = void>
+    class Resource;
+
+    class TextureAsset;
+
+    class PLUTO_API FontAsset final : public Asset
+    {
+    public:
+        class PLUTO_API Glyph
+        {
+        };
+
+        class PLUTO_API Factory final : public Asset::Factory
+        {
+        public:
+            explicit Factory(ServiceCollection& serviceCollection);
+            std::unique_ptr<Asset> Create(FileReader& fileReader) const override;
+        };
+
+    private:
+        class Impl;
+        std::unique_ptr<Impl> impl;
+
+    public:
+        ~FontAsset() override;
+
+        explicit FontAsset(const std::unique_ptr<Impl>& impl);
+
+        FontAsset(const FontAsset& other) = delete;
+        FontAsset(FontAsset&& other) noexcept;
+        FontAsset& operator=(const FontAsset& rhs) = delete;
+        FontAsset& operator=(FontAsset&& rhs) noexcept;
+
+        const Guid& GetId() const override;
+        const std::string& GetName() const override;
+        void SetName(const std::string& value) override;
+        void Dump(FileWriter& fileWriter) const override;
+
+        const Glyph& GetGlyph(char character) const;
+        bool HasCharacter(char character) const;
+
+        Resource<TextureAsset> GetTexture() const;
+    };
+}
