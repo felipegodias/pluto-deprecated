ac20f02bb0d0783a75c690c6afc09691d7ee53c0
felipegodias <felipe.g.o.dias@outlook.com>
2019-08-14 02:21:26 -0300

    Base implementation for the font asset compiler.

--- tools/pluto_asset_manager/font/font_asset_compiler.cpp
+++ tools/pluto_asset_manager/font/font_asset_compiler.cpp
@@ -1,16 +1,63 @@
 #include "font_asset_compiler.h"
+#include "pluto/file/file_reader.h"
+#include "pluto/file/file_manager.h"
+#include "pluto/file/path.h"
+
+#include <array>
+#include <vector>
+
+#define STB_TRUETYPE_IMPLEMENTATION
+#include <stb_truetype.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 
 namespace pluto
 {
     FontAssetCompiler::~FontAssetCompiler() = default;
 
-    FontAssetCompiler::FontAssetCompiler() = default;
+    FontAssetCompiler::FontAssetCompiler(FileManager& fileManager)
+        : fileManager(&fileManager)
+    {
+    }
 
     FontAssetCompiler::FontAssetCompiler(FontAssetCompiler&& other) noexcept = default;
 
     FontAssetCompiler& FontAssetCompiler::operator=(FontAssetCompiler&& rhs) noexcept = default;
 
-    void FontAssetCompiler::Create()
+    std::unique_ptr<FontAsset> FontAssetCompiler::Compile(const Path& path)
     {
+        std::unique_ptr<FileReader> reader = fileManager->OpenRead(path);
+        std::vector<unsigned char> bytes(reader->GetSize());
+        reader->Read(bytes.data(), bytes.size());
+
+        const char first = 32; // Space;
+        const char last = 127; // Del
+        const float fontSize = 64;
+        const uint16_t bitmapWidth = 512;
+        const uint16_t bitmapHeight = 512;
+
+        std::array<stbtt_bakedchar, last - first + 1> bakedChars{};
+        std::vector<uint8_t> bitmap(bitmapWidth * bitmapHeight);
+        stbtt_BakeFontBitmap(bytes.data(), 0, fontSize, bitmap.data(), bitmapWidth, bitmapHeight, first,
+                             bakedChars.size(),
+                             bakedChars.data());
+
+        std::vector<FontAsset::Glyph> glyphs(bakedChars.size());
+        for (size_t i = 0; i < glyphs.size(); ++i)
+        {
+            glyphs[i].character = static_cast<char>(first + i);
+            glyphs[i].xMin = bakedChars[i].x0;
+            glyphs[i].yMin = bakedChars[i].y0;
+            glyphs[i].xMax = bakedChars[i].x1;
+            glyphs[i].yMax = bakedChars[i].y1;
+            glyphs[i].xBearing = bakedChars[i].xoff;
+            glyphs[i].yBearing = bakedChars[i].yoff;
+            glyphs[i].advance = bakedChars[i].xadvance;
+        }
+
+        std::unique_ptr<FontAsset> fontAsset = fontAssetFactory->Create(fontSize, glyphs, bitmapWidth, bitmapHeight,
+                                                                        bitmap);
+        return fontAsset;
     }
 }
--- tools/pluto_asset_manager/font/font_asset_compiler.h
+++ tools/pluto_asset_manager/font/font_asset_compiler.h
@@ -1,18 +1,27 @@
 #pragma once
 
+#include "pluto/asset/font_asset.h"
+#include <memory>
+
 namespace pluto
 {
+    class FileManager;
+    class Path;
+
     class FontAssetCompiler
     {
+        FileManager* fileManager;
+        FontAsset::Factory* fontAssetFactory;
+
     public:
         ~FontAssetCompiler();
-        FontAssetCompiler();
+        explicit FontAssetCompiler(FileManager& fileManager);
 
         FontAssetCompiler(const FontAssetCompiler& other) = delete;
         FontAssetCompiler(FontAssetCompiler&& other) noexcept;
         FontAssetCompiler& operator=(const FontAssetCompiler& rhs) = delete;
         FontAssetCompiler& operator=(FontAssetCompiler&& rhs) noexcept;
 
-        void Create();
+        std::unique_ptr<FontAsset> Compile(const Path& path);
     };
 }
