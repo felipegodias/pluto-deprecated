2aa1038831b3e9a9fc5f6f910f25416bc7f2349f
felipegodias <felipe.g.o.dias@outlook.com>
2019-08-21 00:52:21 -0300

    Added font asset factory to service collection.

--- pluto/src/pluto/asset/asset_installer.cpp
+++ pluto/src/pluto/asset/asset_installer.cpp
@@ -1,5 +1,6 @@
 #include <pluto/asset/asset_installer.h>
 #include <pluto/asset/asset_manager.h>
+#include <pluto/asset/font_asset.h>
 #include <pluto/asset/package_manifest_asset.h>
 #include <pluto/asset/text_asset.h>
 #include <pluto/asset/mesh_asset.h>
@@ -12,12 +13,14 @@ namespace pluto
 {
     void AssetInstaller::Install(ServiceCollection& serviceCollection)
     {
-        serviceCollection.AddFactory<PackageManifestAsset>(std::make_unique<PackageManifestAsset::Factory>(serviceCollection));
+        serviceCollection.AddFactory<PackageManifestAsset>(
+            std::make_unique<PackageManifestAsset::Factory>(serviceCollection));
         serviceCollection.AddFactory<MeshAsset>(std::make_unique<MeshAsset::Factory>(serviceCollection));
         serviceCollection.AddFactory<TextAsset>(std::make_unique<TextAsset::Factory>(serviceCollection));
         serviceCollection.AddFactory<ShaderAsset>(std::make_unique<ShaderAsset::Factory>(serviceCollection));
         serviceCollection.AddFactory<MaterialAsset>(std::make_unique<MaterialAsset::Factory>(serviceCollection));
         serviceCollection.AddFactory<TextureAsset>(std::make_unique<TextureAsset::Factory>(serviceCollection));
+        serviceCollection.EmplaceFactory<FontAsset, FontAsset::Factory>(serviceCollection);
 
         serviceCollection.AddService(AssetManager::Factory(serviceCollection).Create());
     }
@@ -25,6 +28,7 @@ namespace pluto
     void AssetInstaller::Uninstall(ServiceCollection& serviceCollection)
     {
         serviceCollection.RemoveService<AssetManager>();
+        serviceCollection.RemoveFactory<FontAsset>();
         serviceCollection.RemoveFactory<TextureAsset>();
         serviceCollection.RemoveFactory<MaterialAsset>();
         serviceCollection.RemoveFactory<ShaderAsset>();
