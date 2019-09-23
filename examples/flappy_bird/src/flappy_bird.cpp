4058e0913f103d7ccafd035bcefc9f55f004dae4
felipegodias <felipe.g.o.dias@outlook.com>
2019-09-22 21:37:22 -0300

    Added support for setup and teardown.

--- examples/flappy_bird/src/flappy_bird.cpp
+++ examples/flappy_bird/src/flappy_bird.cpp
@@ -1,31 +1,37 @@
 #include <pluto/pluto.h>
 
 #include "components/ground.h"
+#include "components/pipe.h"
 #include "components/flappy_animation.h"
 #include "components/flappy_controller.h"
 #include "managers/game_manager.h"
 
 using namespace pluto;
 
-void Bind(ServiceCollection& serviceCollection)
+void OnSetup(ServiceCollection& serviceCollection)
 {
+    auto& assetManager = serviceCollection.GetService<AssetManager>();
+    assetManager.LoadPackage("flappy_bird");
+
     serviceCollection.EmplaceFactory<Ground>();
+    serviceCollection.EmplaceFactory<Pipe>();
     serviceCollection.EmplaceFactory<FlappyAnimation>();
     serviceCollection.EmplaceFactory<FlappyController>();
     serviceCollection.AddService<GameManager>(GameManager::Factory(serviceCollection).Create());
 }
 
-void OnInit(ServiceCollection& serviceCollection)
+void OnTeardown(ServiceCollection& serviceCollection)
 {
-    auto& assetManager = serviceCollection.GetService<AssetManager>();
-    assetManager.LoadPackage("flappy_bird");
-
-    Bind(serviceCollection);
+    serviceCollection.RemoveService<GameManager>();
+    serviceCollection.RemoveFactory<FlappyController>();
+    serviceCollection.RemoveFactory<FlappyAnimation>();
+    serviceCollection.RemoveFactory<Pipe>();
+    serviceCollection.RemoveFactory<Ground>();
 }
 
 int main(int argc, char* argv[])
 {
     const Root pluto("config.yml", "sandbox.log", "C:/Users/felipe.dias/Repos/flappy_bird");
-    const int exitCode = pluto.Run(&OnInit);
+    const int exitCode = pluto.Run(&OnSetup, &OnTeardown);
     return exitCode;
 }
